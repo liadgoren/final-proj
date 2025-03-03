63c439236ae26a0e21a4f34ab90f0ece8b89fede
Liad <gorenliad1@gmail.com>
2025-03-03 20:21:19 +0200

    test

--- appflask/app.py
+++ appflask/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
-def helloo_name(name):
+def hello_name(name):
    return 'Hello %s!\n' % name
 
 if __name__ == '__main__':
