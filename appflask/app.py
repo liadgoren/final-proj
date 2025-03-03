b9f93cb53eb255ce55564c97a633083eab297da5
Liad <gorenliad1@gmail.com>
2025-03-03 18:06:32 +0200

    test2

--- appflask/app.py
+++ appflask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello_name(name):
-   return 'Helloo %s!\n' % name
+   return 'Hellooo %s!\n' % name
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0")
