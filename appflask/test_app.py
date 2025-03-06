fc27b4fe4cac8732aa4b0302f7d36234509ad9d7
Liad <gorenliad1@gmail.com>
2025-03-06 14:10:27 +0200

    test

--- appflask/test_app.py
+++ appflask/test_app.py
@@ -1,7 +1,5 @@
 import unittest
 from app import app  
-import random
-import string
 
 class HelloNameTestCase(unittest.TestCase):
     def setUp(self):       
@@ -9,13 +7,9 @@ class HelloNameTestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_hello_name(self):       
-        test_input = ''.join(random.choices(string.ascii_letters + ' ', k=12)).strip()  # יצירת טקסט דינמי
-        response = self.app.get(f'/hello/{test_input}')
+        response = self.app.get('/hello/testuser')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Hello testuser!', response.data)
 
-        response_text = response.data.decode()
-        for word in test_input.split():  # בדיקה שכל מילה מהקלט נמצאת בתשובה
-            self.assertIn(word, response_text)
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     unittest.main()
